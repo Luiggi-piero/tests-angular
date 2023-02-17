45d73a3c5f06e46b94e7e00a9e887460a2bcb880
Luiggi Yantas Inocente <luiggiyantas@gmail.com>
2023-02-17 17:21:09 -0500

    medicosComponent

--- src/app/intermedio/espias/medicos.component.spec.ts
+++ src/app/intermedio/espias/medicos.component.spec.ts
@@ -1,5 +1,5 @@
 
-import { EMPTY, from } from 'rxjs';
+import { EMPTY, from, throwError } from 'rxjs';
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
 
@@ -35,5 +35,42 @@ describe('MedicosComponent', () => {
 
         componente.agregarMedico();
         expect(espia).toHaveBeenCalled();
+    });
+
+    it('Debe agregar un nuevo medico al arreglo de médicos', () => {
+        const medico = {id: 1, nombre: 'medico n'};
+
+        spyOn(medicoService, 'agregarMedico').and.returnValue(from([medico]));
+
+        componente.agregarMedico();
+
+        expect(componente.medicos.indexOf(medico)).toBeGreaterThanOrEqual(0);
+    });
+
+    it('Si falla el ingreso del médico, la propiedad mensajeError debe ser igual al error del servicio', () => {
+        const miError = 'No se logró agregar el médico';
+
+        spyOn(medicoService, 'agregarMedico').and.returnValue(throwError(() => miError));
+
+        componente.agregarMedico();
+        expect(componente.mensajeError).toBe(miError);
+    });
+
+    it('Debe de llamar al servidor para borrar un médico', () => {
+
+        spyOn(window, 'confirm').and.returnValue(true);
+
+        const espia = spyOn(medicoService, 'borrarMedico').and.callFake(() => EMPTY);
+        componente.borrarMedico('3');
+        expect(espia).toHaveBeenCalledWith('3');
+    });
+
+    it('NO debe de llamar al servidor para borrar un médico', () => {
+
+        spyOn(window, 'confirm').and.returnValue(false);
+        const espia = spyOn(medicoService, 'borrarMedico').and.callFake(() => EMPTY);
+
+        componente.borrarMedico('3');
+        expect(espia).not.toHaveBeenCalledWith('3');
     });
 });
